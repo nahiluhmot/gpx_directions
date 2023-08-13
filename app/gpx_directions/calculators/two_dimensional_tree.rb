982266d664631fa992999fdcbe68b54c655cd689
Tom Hulihan <hulihan.tom159@gmail.com>
2023-08-13 00:43:06 -0400

    Optimize 2D tree

--- app/gpx_directions/calculators/two_dimensional_tree.rb
+++ app/gpx_directions/calculators/two_dimensional_tree.rb
@@ -23,14 +23,16 @@ module GpxDirections
       def find_nearest_node(lat, lon)
         return if @nodes.empty?
 
-        leaf_idx = find_matching_leaf_idx(lat, lon)
-        to_consider = tree_ancestors(leaf_idx)
-        already_considered = Set.new
+        to_consider = [0]
         best_node = nil
         best_distance = Float::INFINITY
 
         until to_consider.empty?
           idx = to_consider.shift
+
+          best_possible_distance = calculate_best_possible_distance(idx, lat, lon)
+          next if best_distance < best_possible_distance
+
           node = @nodes[idx]
           distance = CoordinateMath.calculate_distance_score(node.lat, node.lon, lat, lon)
 
@@ -39,15 +41,8 @@ module GpxDirections
             best_distance = distance
           end
 
-          [left(idx), right(idx)].each do |cidx|
-            next if already_considered.member?(cidx)
-
-            best_possible_distance = calculate_best_possible_distance(cidx, lat, lon)
-
-            to_consider << cidx if best_possible_distance < best_distance
-          end
-
-          already_considered << node.id
+          to_consider << left(idx)
+          to_consider << right(idx)
         end
 
         best_node
@@ -55,14 +50,16 @@ module GpxDirections
 
       def insert(node_to_insert)
         idx = 0
+        consider_lat = true
 
         while (node_in_tree = @nodes[idx])
           go_left =
-            consider_lat?(idx) ?
+            consider_lat ?
               (node_in_tree.lat > node_to_insert.lat) :
               (node_in_tree.lon > node_to_insert.lon)
 
           idx = go_left ? left(idx) : right(idx)
+          consider_lat = !consider_lat
         end
 
         @nodes[idx] = node_to_insert
@@ -74,61 +71,28 @@ module GpxDirections
         return self if nodes.empty?
 
         nodes = nodes.dup
-        to_insert = [[0, nodes.length - 1, 0]]
+        to_insert = [[0, nodes.length - 1, true]]
 
         until to_insert.empty?
-          start_idx, end_idx, level = to_insert.pop
+          start_idx, end_idx, consider_lat = to_insert.pop
 
           next if start_idx > end_idx
 
-          comparator = level.even? ? :lat : :lon
+          comparator = consider_lat ? :lat : :lon
           idx = (start_idx + end_idx) / 2
           Sorting.quick_select!(nodes, start_idx, end_idx, idx, &comparator)
 
           median = nodes[idx]
-
           insert(median)
 
-          to_insert << [start_idx, idx - 1, level + 1]
-          to_insert << [idx + 1, end_idx, level + 1]
+          to_insert << [start_idx, idx - 1, !consider_lat]
+          to_insert << [idx + 1, end_idx, !consider_lat]
         end
 
         self
       end
 
       private
-
-      def find_matching_leaf_idx(lat, lon)
-        idx = 0
-
-        loop do
-          ridx = right(idx)
-          lidx = left(idx)
-          lnode = @nodes[lidx]
-          rnode = @nodes[ridx]
-
-          break if lnode.nil? && rnode.nil?
-
-          idx =
-            if lnode.nil?
-              ridx
-            elsif rnode.nil?
-              lidx
-            elsif consider_lat?(idx)
-              if @nodes[idx].lat > lat
-                ridx
-              else
-                lidx
-              end
-            elsif @nodes[idx].lon > lon
-              ridx
-            else
-              lidx
-            end
-        end
-
-        idx
-      end
 
       def calculate_best_possible_distance(idx, lat, lon)
         return Float::INFINITY unless @nodes.key?(idx)
@@ -146,53 +110,40 @@ module GpxDirections
         )
       end
 
-      def calculate_lat_lon_bounds(idx)
-        tree_ancestors(idx).reverse_each.each_cons(2) do |parent_idx, child_idx|
-          next if @bounds_by_index.key?(child_idx)
+      def calculate_lat_lon_bounds(child_idx)
+        return @bounds_by_index[child_idx] if @bounds_by_index.key?(child_idx)
 
-          parent = @nodes[parent_idx]
-          bounds = @bounds_by_index[parent_idx].dup
-          @bounds_by_index[child_idx] = bounds
+        parent_idx = up(child_idx)
+        parent = @nodes[parent_idx]
+        bounds = @bounds_by_index[parent_idx].dup
 
-          if consider_lat?(parent_idx)
-            if child_idx.odd?
-              bounds.max_lat = bounds.max_lat.clamp(-Float::INFINITY, parent.lat)
-            end
-
-            if child_idx.even?
-              bounds.min_lat = bounds.min_lat.clamp(parent.lat, Float::INFINITY)
-            end
-          elsif child_idx.odd?
-            bounds.max_lon = bounds.max_lon.clamp(-Float::INFINITY, parent.lon)
-          elsif child_idx.even?
-            bounds.min_lon = bounds.min_lon.clamp(parent.lon, Float::INFINITY)
+        if consider_lat?(parent_idx)
+          if child_idx.odd?
+            bounds.max_lat = bounds.max_lat.clamp(-Float::INFINITY, parent.lat)
           end
+
+          if child_idx.even?
+            bounds.min_lat = bounds.min_lat.clamp(parent.lat, Float::INFINITY)
+          end
+        elsif child_idx.odd?
+          bounds.max_lon = bounds.max_lon.clamp(-Float::INFINITY, parent.lon)
+        elsif child_idx.even?
+          bounds.min_lon = bounds.min_lon.clamp(parent.lon, Float::INFINITY)
         end
 
-        @bounds_by_index[idx]
-      end
-
-      def tree_ancestors(idx)
-        ancestors = []
-        ancestor_idx = idx
-
-        loop do
-          ancestors << ancestor_idx
-
-          break if ancestor_idx.zero?
-
-          ancestor_idx = up(ancestor_idx)
-        end
-
-        ancestors
+        @bounds_by_index[child_idx] = bounds
       end
 
       def consider_lat?(idx)
-        tree_level(idx).even?
-      end
+        level = 0
 
-      def tree_level(idx)
-        Math.log2(idx + 1).floor
+        until idx.zero?
+          idx = up(idx)
+
+          level += 1
+        end
+
+        level.even?
       end
 
       def up(idx)
